078495c2a9e34348b01444e033e2aff83813390a
Fernando Diaz <fdiaz@gitlab.com>
2020-05-20 16:28:39 -0400

    Add AWS Creds

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 import os
 from notes import note
 
+
+# TODO: access aws instance with these
+aws_key_id = "AKIAIOSFODNN7EXAMPLE"
+aws_key_secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     note.run(host='0.0.0.0', port=port)
